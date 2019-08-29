0ab2b4e4767643e2b997674f79eb6bd74c9c4949
Thiago Negreiros (BSC - IT) <thiago_vieira@cal-comp.com.br>
2019-08-29 14:17:38 -0300

    To up class 10

--- calculadora/scripts/controller/CalcController.js
+++ calculadora/scripts/controller/CalcController.js
@@ -3,11 +3,13 @@ class CalcController{
     constructor()
     {
         //This - Reference of Atributes and Methods
+        this._operation    = []; 
         this._displayCalEl = document.querySelector("#display");
         this._dateEl       = document.querySelector("#data");
         this._timeEl       = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
 
         /*
@@ -37,6 +39,151 @@ class CalcController{
 
         }, 1000);
 
+    }
+
+    //Eventos
+    addEventListenerAll(element,events, fn)
+    {
+        events.split(" ").forEach(event => 
+        {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll()
+    {
+        this._operation = [];
+    }
+
+    clearEntry()
+    {
+        this._operation.pop();
+    }
+
+    getLastOperation()
+    {
+        return this._operation[this._operation.length-1];
+    }
+
+    setLastOperator(value)
+    {
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value)
+    {
+        return (["+", "-", "*", "%"].indexOf(value) > -1);
+        
+    }
+
+    addOperation(value)
+    {
+        if(isNaN(this.getLastOperation()))
+        {
+            //String
+            if(this.isOperator(value))
+            {
+                this.setLastOperator(value);
+            }
+            else if(isNaN(value))
+            {
+                console.log(value);
+            }
+            else
+            {
+                this._operation.push(value);
+            }
+        }
+        else
+        {
+            //Number
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperator(parseInt(newValue));
+        }
+
+
+        this._operation.push(value);
+
+        console.log(this._operation);
+    }
+
+    setError()
+    {
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value)
+    {
+        switch (value)
+        {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.addOperation("+");
+                break;
+            case 'subtracao':
+                    this.addOperation("-");
+                break;
+            case 'multiplicacao':
+                    this.addOperation("*");
+                break;
+            case 'divisao':
+                    this.addOperation("/");
+                break;
+            case 'porcento':
+                    this.addOperation("%");
+                break;
+            case 'igual':
+                    
+                break;
+
+            case 'ponto':
+                    this.addOperation(".");
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
+    }
+
+
+    //Vai iniciar os eventos do botão
+    initButtonsEvents()
+    {
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g"); 
+        
+        buttons.forEach((btn, index) => 
+        {
+            this.addEventListenerAll(btn, 'click drag', e => 
+            {
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>
+            {
+                btn.style.cursor = "pointer";
+            });
+        });
     }
     
     setDisplayDateTime()
