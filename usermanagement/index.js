e0fe6937415fafcc00957d413ea863351941557f
Thiago Negreiros (BSC - IT) <thiago_vieira@cal-comp.com.br>
2019-09-30 14:24:10 -0300

    G4

--- usermanagement/index.js
+++ usermanagement/index.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+let name     = document.querySelector("#exampleInputName");
+let genre    = document.querySelectorAll("#form-user-create [name=gender]:checked");
+let birth    = document.querySelector("#exampleInputBirth");
+let country  = document.querySelector("#exampleInputCountry");
+let email    = document.querySelector("#exampleInputEmail");
+let password = document.querySelector("#exampleInputPassword");
+let photo    = document.querySelector("#exampleInputFile");
+let admin    = document.querySelector("#exampleInputAdmin");
