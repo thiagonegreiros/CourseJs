8e3ff2a50d398c63373b464b332ad5ec707e6d95
Thiago Negreiros (BSC - IT) <thiago_vieira@cal-comp.com.br>
2019-10-07 09:20:03 -0300

    G26

--- usermanagement/controllers/UserController.js
+++ usermanagement/controllers/UserController.js
@@ -237,7 +237,7 @@ class UserController
                     <td>${Utils.dateFormat(dataUser.register)}</td>
                     <td>
                     <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                     </td>
                 </tr>
         `;
@@ -252,6 +252,18 @@ class UserController
 
     addEventsTr(tr)
     {
+
+        // Faz a função de excluir os dados da tela
+        tr.querySelector(".btn-delete").addEventListener("click", e => 
+        {
+            if(confirm("Deseja realmente excluir?"))
+            {
+                tr.remove();
+                this.updateCount();
+            }
+        });
+
+        // Faz a função de alterar os dados da tela
         tr.querySelector(".btn-edit").addEventListener("click", e=>
         {
             let json = JSON.parse(tr.dataset.user);
